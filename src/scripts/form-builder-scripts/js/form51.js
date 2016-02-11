c73b8a49f2ba1b8210718debadd3da57bfa12a3e
Joseph Wallingford <wallyjaw@gmail.com>
2016-02-11 12:00:33 -0700

    some missed files

---
+++ src/scripts/form-builder-scripts/js/form51.js
@@ -0,0 +1,57 @@
+define(['jquery'], function($) {
+  var state = true;
+
+  var queryString = function() {
+    var queryString = {};
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+
+    for (var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split("=");
+
+      if (typeof queryString[pair[0]] === "undefined") {
+        queryString[pair[0]] = decodeURIComponent(pair[1]);
+      } else if (typeof queryString[pair[0]] === "string") {
+        var arr = [queryString[pair[0]], decodeURIComponent(pair[1])];
+        queryString[pair[1]] = arr;
+      } else {
+        queryString[pair[0]].push(decodeURIComponent(pair[1]));
+      }
+    }
+
+    return queryString;
+  }
+
+  var drawAttentionToElement = function() {
+    var $element = $('.pdf_sped-teacher');
+    if ($element.find('input').length > 0) {
+      scope.formContent.originalElements.forEach(function(item, index) {
+        if (item.class == "pdf_sped-teacher") {
+          item.description = "(case manager)";
+          scope.$digest();
+        }
+      });
+
+      if (state) {
+        state = false;
+        $element.animate({
+          backgroundColor: "#fcf8e3"
+        }, 3000);
+        setTimeout(drawAttentionToElement, 15000);
+        $element.find('input').focus();
+      } else {
+        $element.animate({
+          backgroundColor: "#fff"
+        }, 3000);
+      }
+    } else {
+      setTimeout(drawAttentionToElement, 1000);
+    }
+  }
+
+	return function() {
+    if (typeof queryString().case_manager !== 'undefined') {
+      drawAttentionToElement();
+    }
+	}
+});
---
+++ src/scripts/iep/blank.js
@@ -0,0 +1,145 @@
+require(['jquery', 'handlebars'], function($, Handlebars) {
+  var API_BASE_URL = 'https://pats.irondistrict.org/';
+  var checkboxes = '.forms-list-container input[type=checkbox]';
+  var checkedCheckboxes = '.forms-list-container input[type=checkbox]:checked';
+
+  $(document).ready(function() {
+    loadForms();
+    $('button[type=submit]').on('click', printSelectedForms);
+  });
+
+  function iCheck() {
+    $(checkboxes).iCheck({
+      checkboxClass: 'icheckbox_square-blue',
+      radioClass: 'iradio_square-blue'
+    });
+
+    $(checkboxes).on('ifChanged', inputWatcher);
+  }
+
+  function toggleSelect(event) {
+    if ($(event.target).text() == 'Select All') {
+      $(checkboxes).iCheck('check');
+    } else {
+      $(checkboxes).iCheck('uncheck');
+    }
+  }
+
+  function inputWatcher(event) {
+    if ($(checkedCheckboxes).length < 1) {
+      $('#btnToggleSelection').text('Select All');
+      $('button[type=submit]').hide();
+    } else {
+      $('button[type=submit]').show();
+    }
+
+    if ($(checkedCheckboxes).length > 1) {
+      $('.options-file').show();
+    } else {
+      $('.options-file').hide();
+    }
+
+    if ($(checkedCheckboxes).length == $(checkboxes).length) {
+      $('#btnToggleSelection').text('Select None');
+    }
+  }
+
+  function loadForms() {
+    $.ajax({
+      url: API_BASE_URL + 'get-blanks',
+      type: 'POST',
+      dataType: 'json',
+      data: {forms: JSON.stringify(forms), action: 'getBlanks'},
+    })
+    .done(function(response) {
+      if (response.length > 0) {
+        $('#btnToggleSelection').show();
+        $('#btnToggleSelection').on('click', toggleSelect);
+        $.each(response, function(index, form) {
+          var source = $('#form-list-item-template').html();
+          var template = Handlebars.compile(source);
+          var html = template(form);
+
+          var listNum = ((index % 3) + 1);
+
+          $('.forms-list:nth-of-type('+listNum+')').append(html);
+        });
+
+        iCheck();
+      } else {
+        $('#btnToggleSelection').after('<p> Sorry, there are no printable blank forms right now. </p>');
+      }
+    })
+    .fail(function(error) {
+      console.log(error);
+      $('#btnToggleSelection').after('<p> Sorry, thre was an error communicating with the server. </p>');
+    });
+  }
+
+  function printSelectedForms(event) {
+    // make sure there are checked forms
+    if ($(checkedCheckboxes).length < 1) {
+      alert('There are no forms selected.'); // TODO: might want to change how to notify
+      return;
+    }
+
+    var selected = [];
+
+    $(checkedCheckboxes).each(function(index, form) {
+      selected.push({
+        id: $(form).val(),
+        title: $(form).parents('li').find('.title').text()
+      });
+    });
+
+    console.log(JSON.stringify(selected));
+
+    if (selected.length > 0) {
+      togglePrintButtonState('disabled');
+
+      $.ajax({
+        url: API_BASE_URL + 'print-blanks',
+        type: 'POST',
+        dataType: 'json',
+        data: {forms: JSON.stringify(selected), action: 'printBlanks'},
+      })
+      .done(function(response) {
+        console.log(response);
+        if (response.file.length > 0) {
+          var win = window.open(API_BASE_URL + response.file, '_blank');
+          if (win) {
+            win.focus();
+          } else {
+            alert('ERROR: Please allow popups for this page.');
+          }
+        }
+
+        for (var key in response.error) {
+          var parentElement = $('input[value='+key+']').parents('li');
+                parentElement.addClass('error');
+                parentElement.find('.form-error').text(response.error[key]);
+              }
+      })
+      .fail(function(data) {
+        console.log('Error');
+        console.log(data);
+      })
+      .always(function() {
+        togglePrintButtonState('enabled');
+      });
+
+    } else {
+      alert('ERROR: selected form\'s data could not be collected');
+    }
+  }
+
+  function togglePrintButtonState(state) {
+    if (state == 'disabled') {
+      $('#btnPrintSelection').prop('disabled', true);
+      $('#btnPrintSelection i').removeClass('hide');
+    } else {
+      $('#btnPrintSelection').prop('disabled', false);
+      $('#btnPrintSelection i').addClass('hide');
+    }
+  }
+});
