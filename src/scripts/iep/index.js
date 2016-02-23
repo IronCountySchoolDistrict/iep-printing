22c3cefb5b5bd11e28088592330eb4d6b2f7829f
Joseph Wallingford <wallyjaw@gmail.com>
2016-02-23 10:02:26 -0700

    removed the logging from the timeout function

--- src/scripts/iep/index.js
+++ src/scripts/iep/index.js
@@ -3,12 +3,10 @@ document.getElementById('iep-frame').src = '/* @echo API_URL */' + loc;
 
 function checkStorage() {
   if (localStorage.getItem('iep')) {
-    console.log('reload iframe');
     document.getElementById('iep-frame').src = document.getElementById('iep-frame').src;
     localStorage.removeItem('iep');
   }
 
-  console.log('waiting...');
   setTimeout(checkStorage, 1000);
 }
 
