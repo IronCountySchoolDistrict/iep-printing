0bd856a88a5422b4bcd58f81aa00963f460f1124
Nathan Jones <nathan.jones@ironmail.org>
2016-08-23 11:33:04 -0600

    Moved and consolidated require.config() calls, use es6 module syntax to import URI

--- src/scripts/form-builder-scripts/js/ajax-helper.js
+++ src/scripts/form-builder-scripts/js/ajax-helper.js
@@ -1,37 +1,28 @@
-require.config({
-  paths: {
-    URI: 'https://cdnjs.cloudflare.com/ajax/libs/URI.js/1.18.0/URI',
-    punycode: 'https://cdnjs.cloudflare.com/ajax/libs/URI.js/1.18.0/punycode',
-    IPv6: 'https://cdnjs.cloudflare.com/ajax/libs/URI.js/1.18.0/IPv6',
-    SecondLevelDomains: 'https://cdnjs.cloudflare.com/ajax/libs/URI.js/1.18.0/SecondLevelDomains'
-  }
-});
+import URI from 'URI';
 
-require(['URI'], URI => {
-  /**
-   * determine if responseid should be inserted into AJAX request
-   * @param  {object} openParams   URI.js object for uri that XHR.open was about to open
-   * @param  {object} windowParams URI.js object for uri of the current page (uses content frame if opened in frame)
-   * @return {boolean}          returns true if responseId should be added to url, false if it shouldn't be inserted
-   */
-  function shouldInsertResponseId(openParams, windowParams) {
-    return (
-      typeof openParams.action !== 'undefined' &&
-      typeof windowParams.responseid !== 'undefined' &&
-      openParams.action === 'getChoices' &&
-      windowParams.responseid > 0
-    );
+/**
+ * determine if responseid should be inserted into AJAX request
+ * @param  {object} openParams   URI.js object for uri that XHR.open was about to open
+ * @param  {object} windowParams URI.js object for uri of the current page (uses content frame if opened in frame)
+ * @return {boolean}          returns true if responseId should be added to url, false if it shouldn't be inserted
+ */
+function shouldInsertResponseId(openParams, windowParams) {
+  return (
+    typeof openParams.action !== 'undefined' &&
+    typeof windowParams.responseid !== 'undefined' &&
+    openParams.action === 'getChoices'
+  );
+}
+
+((open) => {
+  XMLHttpRequest.prototype.open = function(method, url, async, user, pass) {
+    var openUri = new URI(url);
+    var windowUri = new URI(window.location);
+    var openParams = openUri.search(true);
+    var windowParams = windowUri.search(true);
+    if (shouldInsertResponseId(openParams, windowParams)) {
+      openUri.setSearch('responseid', windowParams.responseid);
+    }
+    open.call(this, method, openUri.toString(), async, user, pass);
   }
-  (function(open) {
-    XMLHttpRequest.prototype.open = function(method, url, async, user, pass) {
-      var openUri = new URI(url);
-      var windowUri = new URI(window.location);
-      var openParams = openUri.search(true);
-      var windowParams = windowUri.search(true);
-      if (shouldInsertResponseId(openParams, windowParams)) {
-        openUri.setSearch('responseid', windowParams.responseid);
-      }
-      open.call(this, method, openUri.toString(), async, user, pass);
-    };
-  }(XMLHttpRequest.prototype.open));
-});
+})(XMLHttpRequest.prototype.open);
